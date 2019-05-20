e8757c1a063aefe501fc04894646da271edf5541
samim <samim@goplannr.com>
2019-05-20 17:42:36 +0530

    UPdated settings

--- crm/settings.py
+++ crm/settings.py
@@ -279,6 +279,7 @@ except ImportError:
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 GP_CLIENT_ID = os.getenv('GP_CLIENT_ID', False)
 GP_CLIENT_SECRET = os.getenv('GP_CLIENT_SECRET', False)
 ENABLE_GOOGLE_LOGIN = os.getenv('ENABLE_GOOGLE_LOGIN', False)
